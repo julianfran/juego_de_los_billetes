1d07c8cd530b850adff85d0674c7389a4b174a95
julianfran <julian_franco@hotmail.com.ar>
2023-04-05 11:56:32 -0300

    Mostrando billetes aleatorios

--- data.js
+++ data.js
@@ -1,164 +1,186 @@
-billetes = [
-    id_1 = {
-        nombre: "Moneda de 1 peso",
-        personaje: "Sol patrio",
-        año: 1994,
-        valor: 1,
-        tipo: "moneda",
-        img:"assets/billetes/moneda_1_a.png"
-    },
-    id_2 = {
-        nombre: "Moneda de 1 peso - Árboles de la República Argentina",
-        personaje: "Jacarandá",
-        año: 2017,
-        valor: 1,
-        tipo: "moneda",
-        img:"assets/billetes/moneda_1_b.png"
-    },
-    id_3 = {
-        nombre: "Moneda de 2 pesos - Bicentenario de la Revolución de Mayo de 1810",
-        personaje:"",
-        año: 2011,
-        valor: 2,
-        tipo: "moneda",
-        img: "assets/billetes/moneda_2_a.png"
-    },
-    id_4 = {
-        nombre: "Moneda de 2 pesos - Árboles de la República Argentina",
-        personaje:"Palo borracho",
-        año: 2018,
-        valor: 2,
-        tipo: "moneda",
-        img: "assets/billetes/moneda_2_b.png"
-    },
-    id_5 = {
-        nombre: "Moneda de 5 pesos - Árboles de la República Argentina",
-        personaje:"Arrayán",
-        año: 2017,
-        valor: 5,
-        tipo: "moneda",
-        img: "assets/billetes/moneda_5.png"
-    },
-    id_6 = {
-        nombre: "Moneda de 10 pesos - Árboles de la República Argentina",
-        personaje:"Caldén",
-        año: 2018,
-        valor: 10,
-        tipo: "moneda",
-        img: "assets/billetes/moneda_10.png"
-    },
-    id_7 = {
-        nombre: "Moneda de 50 centavos",
-        personaje:"",
-        año: 1992,
-        valor: 0.5,
-        tipo: "moneda",
-        img: "assets/billetes/moneda_50.png"
-    },
-    id_8 = {
-        nombre: "Billete de 10 pesos",
-        personaje:"Belgrano",
-        año: 1998,
-        valor: 10,
-        tipo: "billete",
-        img: "assets/billetes/10a.jpg"
-    },
-    id_9 = {
-        nombre: "Billete de 10 pesos",
-        personaje: "Belgrano",
-        año: 1998,
-        valor: 10,
-        tipo: "billete",
-        img: "assets/billetes/10b.jpg"
-    },
-       id_10 = {
-        nombre: "Billete de 20 pesos",
-        personaje: "Juan Manuel de Rosas",
-        año: 2000,
-        valor: 20,
-        tipo: "billete",
-        img: "assets/billetes/20a.jpg"
-    },
-    id_11 = {
-        nombre: "Billete de 20 pesos",
-        personaje: "Guanaco",
-        año: 2017,
-        valor: 20,
-        tipo: "billete",
-        img: "assets/billetes/20a.jpg"
-    },
-      id_12 = {
-        nombre: "Billete de 50 pesos",
-        personaje: "Domingo Sarmiento",
-        año: 1999,
-        valor: 50,
-        tipo: "billete",
-        img: "assets/billetes/50a.jpg"
-    },
-    id_13 = {
-        nombre: "Billete de 50 pesos",
-        personaje: "Islas Malvinas",
-        año: 2015,
-        valor: 50,
-        tipo: "billete",
-        img: "assets/billetes/50b.jpg"
-    },
-     id_14 = {
-        nombre: "Billete de 50 pesos",
-        personaje: "Cóndor Andino",
-        año: 2018,
-        valor: 50,
-        tipo: "billete",
-        img: "assets/billetes/50c.jpg"
-    },
-    id_15 = {
-        nombre: "Billete de 100 pesos",
-        personaje: "Roca",
-        año: 1999,
-        valor: 100,
-        tipo: "billete",
-        img: "assets/billetes/100a.jpg"
-    },
-    id_16 = {
-        nombre: "Billete de 100 pesos",
-        personaje: "Evita",
-        año: 2012,
-        valor: 100,
-        tipo: "billete",
-        img: "assets/billetes/100b.jpg"
-    },
-    id_17 = {
-        nombre: "Billete de 100 pesos",
-        personaje: "Taruca",
-        año: 2018,
-        valor: 100,
-        tipo: "billete",
-        img: "assets/billetes/100c.jpg"
-    },
-    id_18 = {
-        nombre: "Billete de 200 pesos",
-        personaje: "Ballena Franca Austral",
-        año: 2016,
-        valor: 200,
-        tipo: "billete",
-        img: "assets/billetes/200.jpg"
-    },
-    id_19 = {
-        nombre: "Billete de 500 pesos",
-        personaje: "Yaguareté",
-        año: 2016,
-        valor: 500,
-        tipo: "billete",
-        img: "assets/billetes/500.jpg"
-    },
-    id_20 = {
-        nombre: "Billete de 1000 pesos",
-        personaje: "Hornero",
-        año: 2017,
-        valor: 1000,
-        tipo: "billete",
-        img: "assets/billetes/1000.jpg"
-    },
+const data = {
+    "billetes": [
+        {
+            _id: 1,
+            "nombre": "Moneda de 1 peso",
+            "personaje": "Sol patrio",
+            "date": 1994,
+            "valor": 1,
+            "tipo": "moneda",
+            "img": "assets/billetes/moneda_1_a.png"
+        },
+        {
+            _id: 2,
+            "nombre": "Moneda de 1 peso - Árboles de la República Argentina",
+            "personaje": "Jacarandá",
+            "date": 2017,
+            "valor": 1,
+            "tipo": "moneda",
+            "img": "assets/billetes/moneda_1_b.png"
+        },
+        {
+            _id: 3,
+            "nombre": "Moneda de 2 pesos - Bicentenario de la Revolución de Mayo de 1810",
+            "personaje": "",
+            "date": 2011,
+            "valor": 2,
+            "tipo": "moneda",
+            "img": "assets/billetes/moneda_2_a.png"
+        },
+        {
+            _id: 4,
+            "nombre": "Moneda de 2 pesos - Árboles de la República Argentina",
+            "personaje": "Palo borracho",
+            "date": 2018,
+            "valor": 2,
+            "tipo": "moneda",
+            "img": "assets/billetes/moneda_2_b.png"
+        },
+        {
+            _id: 5,
+            "nombre": "Moneda de 5 pesos - Árboles de la República Argentina",
+            "personaje": "Arrayán",
+            "date": 2017,
+            "valor": 5,
+            "tipo": "moneda",
+            "img": "assets/billetes/moneda_5.png"
+        },
+        {
+            _id: 6,
+            "nombre": "Moneda de 10 pesos - Árboles de la República Argentina",
+            "personaje": "Caldén",
+            "date": 2018,
+            "valor": 10,
+            "tipo": "moneda",
+            "img": "assets/billetes/moneda_10.png"
+        },
+        {
+            _id: 7,
+            "nombre": "Moneda de 50 centavos",
+            "personaje": "",
+            "date": 1992,
+            "valor": 0.5,
+            "tipo": "moneda",
+            "img": "assets/billetes/moneda_50.png"
+        },
+        {
+            _id: 8,
+            "nombre": "Billete de 10 pesos",
+            "personaje": "Belgrano",
+            "date": 1998,
+            "valor": 10,
+            "tipo": "billete",
+            "img": "assets/billetes/10a.jpg"
+        },
+        {
+            _id: 9,
+            "nombre": "Billete de 10 pesos",
+            "personaje": "Belgrano",
+            "date": 1998,
+            "valor": 10,
+            "tipo": "billete",
+            "img": "assets/billetes/10b.jpg"
+        },
+        {
+            _id: 10,
+            "nombre": "Billete de 20 pesos",
+            "personaje": "Juan Manuel de Rosas",
+            "date": 2000,
+            "valor": 20,
+            "tipo": "billete",
+            "img": "assets/billetes/20a.jpg"
+        },
+        {
+            _id: 11,
+            "nombre": "Billete de 20 pesos",
+            "personaje": "Guanaco",
+            "date": 2017,
+            "valor": 20,
+            "tipo": "billete",
+            "img": "assets/billetes/20a.jpg"
+        },
+        {
+            _id: 12,
+            "nombre": "Billete de 50 pesos",
+            "personaje": "Domingo Sarmiento",
+            "date": 1999,
+            "valor": 50,
+            "tipo": "billete",
+            "img": "assets/billetes/50a.jpg"
+        },
+        {
+            _id: 13,
+            "nombre": "Billete de 50 pesos",
+            "personaje": "Islas Malvinas",
+            "date": 2015,
+            "valor": 50,
+            "tipo": "billete",
+            "img": "assets/billetes/50b.jpg"
+        },
+        {
+            _id: 14,
+            "nombre": "Billete de 50 pesos",
+            "personaje": "Cóndor Andino",
+            "date": 2018,
+            "valor": 50,
+            "tipo": "billete",
+            "img": "assets/billetes/50c.jpg"
+        },
+        {
+            _id: 15,
+            "nombre": "Billete de 100 pesos",
+            "personaje": "Roca",
+            "date": 1999,
+            "valor": 100,
+            "tipo": "billete",
+            "img": "assets/billetes/100a.jpg"
+        },
+        {
+            _id: 16,
+            "nombre": "Billete de 100 pesos",
+            "personaje": "Evita",
+            "date": 2012,
+            "valor": 100,
+            "tipo": "billete",
+            "img": "assets/billetes/100b.jpg"
+        },
+        {
+            _id: 17,
+            "nombre": "Billete de 100 pesos",
+            "personaje": "Taruca",
+            "date": 2018,
+            "valor": 100,
+            "tipo": "billete",
+            "img": "assets/billetes/100c.jpg"
+        },
+        {
+            _id: 18,
+            "nombre": "Billete de 200 pesos",
+            "personaje": "Ballena Franca Austral",
+            "date": 2016,
+            "valor": 200,
+            "tipo": "billete",
+            "img": "assets/billetes/200.jpg"
+        },
+        {
+            _id: 19,
+            "nombre": "Billete de 500 pesos",
+            "personaje": "Yaguareté",
+            "date": 2016,
+            "valor": 500,
+            "tipo": "billete",
+            "img": "assets/billetes/500.jpg"
+        },
+        {
+            _id: 20,
+            "nombre": "Billete de 1000 pesos",
+            "personaje": "Hornero",
+            "date": 2017,
+            "valor": 1000,
+            "tipo": "billete",
+            "img": "assets/billetes/1000.jpg"
+        }
+    ]
 
 
-]
+}
--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const saludo = document.getElementById('saludo')
 const contenedorBienvenida = document.getElementById('contenedorBienvenida')
 const bienvenida = document.getElementById('bienvenida')
 const contenedorBilletes = document.getElementById('contenedorBilletes')
+const contenedor = document.getElementById('contenedor')
+
+
+//Elementos aleatorios
+const dataBilletes = data.billetes
+const billeteAleatorio1 = dataBilletes[Math.floor(Math.random() * data.billetes.length)]
+const billeteAleatorio2 = dataBilletes[Math.floor(Math.random() * data.billetes.length)]
+console.log(billeteAleatorio1)
+console.log(billeteAleatorio2.img)
+
+
 
 // Borrar elemento hijo
 const borrarDiv = ((padre, hijo) => {
@@ -24,12 +35,32 @@ let imprimirNombre = (() => {
                     <div id="saludo">
                     <h2>¡Hola ${jugador}!</h2>
                     <p>Vamos con el primer desafío:</p>
-                    </div>`
+                    </div>
+                  `
   borrarDiv(contenedor, contenedorBienvenida)
-  main.innerHTML = imprimir
+  contenedor.innerHTML = imprimir
 })
 
-let mostrarBilletes = (()=>{
-  
+let mostrarBilletes = (() => {
+  let imprimir = `
+                    <div id="opcion1">
+                      <h3></h3>
+                      <img src="${billeteAleatorio1.img}" alt="">
+                    </div>
+                    <div id="opcion2">
+                      <h3></h3>
+                       <img src="${billeteAleatorio2.img}" alt="">
+                    </div>
+                  `;
+  contenedorBilletes.innerHTML = imprimir
 })
+
+const play = (() =>{
+imprimirNombre()
+mostrarBilletes()
+})
+
+
+
+
 
